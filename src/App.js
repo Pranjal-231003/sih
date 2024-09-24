010731b91704113ad86fa11b85b025a6dd386b2a
Priyanshu Jain <jainpriyanshu324@gmail.com>
2024-09-24 14:31:19 +0530

    adding navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login/Login';
-import SignUp from './components/Signup/SignUp';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login/Login";
+import SignUp from "./components/Signup/SignUp";
+import Navbar from "./components/Navbar/Navbar.js";
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
       {/* <SignUp /> */}
+      <Navbar />
     </div>
   );
 }
